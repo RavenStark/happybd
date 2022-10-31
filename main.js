a668a2d8d614e530793b7039d9cd11e31737a9bc
RavenStark <107474741+RavenStark@users.noreply.github.com>
2022-10-31 14:46:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var canvas = new fabric.Canvas('myCanvas');
+ctx = canvas.getContext("2d");
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+    fabric.Image.fromURL('birthday.png', function(Img){
+        block_image_object = Img;
+        
+        block_image_object.scaleTowidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:0
+        });
+        
+        canvas.add(block_image_object);
+        
+            });
+}
+function playSound() {
+	x.play();
+}
